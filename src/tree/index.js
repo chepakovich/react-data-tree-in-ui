7afd9f015efa0cd7d6ce61bb767f994ca978e667
chepakovich <chepakovich@gmail.com>
2022-01-29 13:30:11 -0800

    added css styling

---
+++ src/tree/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+export default function Tree() {
+  return (
+    <div className="tree">
+      <p className="level0">root</p>
+      <p className="level1">ant</p>
+      <p className="level1">bear</p>
+      <p className="level2">cat</p>
+      <p className="level2">dog</p>
+      <p className="level3">elephant</p>
+      <p className="level1">frog</p>
+    </div>
+  );
+}
