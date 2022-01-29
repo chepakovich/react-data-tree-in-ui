f65a573021f7464fa2a1a85e5af40955c270ecf9
chepakovich <chepakovich@gmail.com>
2022-01-29 13:40:57 -0800

    prove that with css the result is exactly the same

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,10 +1,26 @@
+import { useEffect, useRef } from 'react';
 import './index.css';
 
 export default function Tree() {
+  const ref1 = useRef();
+  const ref2 = useRef();
+
+  useEffect(() => {
+    const position1 = ref1.current.getBoundingClientRect();
+    const position2 = ref2.current.getBoundingClientRect();
+    console.log('position1:', position1.x);
+    console.log('position2:', position2.x);
+  }, []);
+
   return (
     <div className="tree">
       <p className="level0">root</p>
-      <p className="level1">ant</p>
+      <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;<span ref={ref1}>ant</span>
+      </p>
+      <p className="level1">
+        <span ref={ref2}>ant</span>
+      </p>
       <p className="level1">bear</p>
       <p className="level2">cat</p>
       <p className="level2">dog</p>
