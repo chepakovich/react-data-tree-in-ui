02b920fb98d7b5059fcb663b52384e95d73ce8ea
chepakovich <chepakovich@gmail.com>
2022-01-29 13:54:10 -0800

    added numbers to every line

--- src/tree/index.js
+++ src/tree/index.js
@@ -6,21 +6,20 @@ export default function Tree() {
   const ref2 = useRef();
 
   useEffect(() => {
-    const position1 = ref1.current.getBoundingClientRect();
-    const position2 = ref2.current.getBoundingClientRect();
-    console.log('position1:', position1.x);
-    console.log('position2:', position2.x);
+    // const position1 = ref1.current.getBoundingClientRect();
+    // const position2 = ref2.current.getBoundingClientRect();
+    // console.log('position1:', position1.x);
+    // console.log('position2:', position2.x);
   }, []);
 
   return (
     <div className="tree">
       <p className="level0">root</p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;<span ref={ref1}>ant</span>
-      </p>
+      {/* <p>&nbsp;&nbsp;&nbsp;&nbsp;<span ref={ref1}>ant</span></p>
       <p className="level1">
         <span ref={ref2}>ant</span>
-      </p>
+      </p> */}
+      <p className="level1">ant</p>
       <p className="level1">bear</p>
       <p className="level2">cat</p>
       <p className="level2">dog</p>
