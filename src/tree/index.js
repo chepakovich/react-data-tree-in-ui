dd63d4c81ae3f008bd58a7cc41b10a13177d88f8
chepakovich <chepakovich@gmail.com>
2022-01-29 14:07:06 -0800

    use JSON object as data source

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,30 +1,65 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './index.css';
+import initialData from './data';
 
 export default function Tree() {
+  const [data, setData] = useState(initialData);
+  const [tree, setTree] = useState([]);
+
   const ref1 = useRef();
   const ref2 = useRef();
+
+  const generateNewTree = (newData) => {
+    const newTree = [];
+    const generateTree = (inputArr, level) => {
+      if (inputArr.length === 0) return;
+      let name;
+      inputArr.forEach((el) => {
+        name = el.name;
+        newTree.push({
+          level,
+          name,
+        });
+        generateTree(el.children, level + 1);
+      });
+      newTree.push({
+        levelEnd: true,
+        level,
+        name,
+      });
+    };
+    generateTree(newData, 0);
+    setTree(newTree);
+  };
 
   useEffect(() => {
     // const position1 = ref1.current.getBoundingClientRect();
     // const position2 = ref2.current.getBoundingClientRect();
     // console.log('position1:', position1.x);
     // console.log('position2:', position2.x);
+
+    generateNewTree(data);
   }, []);
 
   return (
     <div className="tree">
-      <p className="level0">root</p>
-      {/* <p>&nbsp;&nbsp;&nbsp;&nbsp;<span ref={ref1}>ant</span></p>
+      {/* <p className="level0">root</p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;<span ref={ref1}>ant</span></p>
       <p className="level1">
         <span ref={ref2}>ant</span>
-      </p> */}
+      </p>
       <p className="level1">ant</p>
       <p className="level1">bear</p>
       <p className="level2">cat</p>
       <p className="level2">dog</p>
       <p className="level3">elephant</p>
-      <p className="level1">frog</p>
+      <p className="level1">frog</p> */}
+
+      {tree.map((item, index) => (
+        <p key={index} className={`level${item.level}`}>
+          {item.name}
+        </p>
+      ))}
     </div>
   );
 }
